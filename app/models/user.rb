2ead2087a19b95118bd4b5923aa6e46183e1dfea
Dimitar Uzunov <dimitar.uzunov.dev@gmail.com>
2016-02-04 22:18:00 +0200

    Add user model

---
+++ app/models/user.rb
@@ -0,0 +1,29 @@
+require 'bcrypt'
+
+class User
+  include Mongoid::Document
+  include BCrypt
+
+  field :email, type: String
+  field :password_hash, type: String
+
+  index({email: 1}, {unique: true, name: 'email_index'})
+
+  validates_presence_of :email, message: 'Email address is required'
+  validates_uniqueness_of :email, message: 'Email address exists'
+  validates_length_of :password, :minimum => 8,
+    message: 'Password must be longer than 8 characters'
+
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
+
+  def self.find_by_email(email)
+    find_by(email: email)
+  end
+end
