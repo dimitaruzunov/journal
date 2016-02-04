938d1d85979ec1512fe45fcb438d7901430c3728
Dimitar Uzunov <dimitar.uzunov.dev@gmail.com>
2016-02-04 22:08:54 +0200

    Enable sessions. Used to implement user authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < Sinatra::Base
   set :views, File.expand_path('../../views', __FILE__)
   set :public_folder, File.expand_path('../../../public', __FILE__)
 
+  enable :sessions
+
   get '/' do
     slim :index
   end
