a2b7496daa0fd2554cec7d93299e86439a858d75
Dimitar Uzunov <dimitar.uzunov.dev@gmail.com>
2016-02-04 23:28:13 +0200

    Add authenticate method and post route

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,5 +1,22 @@
 class LoginController < ApplicationController
   get '/' do
+    redirect to '../' if authenticated?
+
     slim :'login/login'
+  end
+
+  post '/' do
+    redirect to '../' if authenticated?
+
+    redirect to '../' if authenticate(params[:email], params[:password])
+
+    redirect to '/'
+  end
+
+  private
+
+  def authenticate(email, password)
+    user = User.find_by_email(email)
+    session[:user] = user.id if user and user.password == password
   end
 end
