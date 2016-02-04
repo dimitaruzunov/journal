6d4f618154604318b95545dda8a821af44eb336c
Dimitar Uzunov <dimitar.uzunov.dev@gmail.com>
2016-02-04 23:33:01 +0200

    Redirect to home if the user has already logged in

--- app/controllers/register_controller.rb
+++ app/controllers/register_controller.rb
@@ -1,13 +1,14 @@
 class RegisterController < ApplicationController
   get '/' do
+    redirect to '../' if authenticated?
+
     slim :'register/register'
   end
 
   post '/' do
-    email = params[:email]
-    password = params[:password]
+    redirect to '../' if authenticated?
 
-    user = User.create(email: email, password: password)
+    user = User.create(email: params[:email], password: params[:password])
     redirect to '../' if user.valid?
 
     redirect to '/'
