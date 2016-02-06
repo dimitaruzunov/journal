21fd918b847d84b5b355789893827ccf2c147f7d
Dimitar Uzunov <dimitar.uzunov.dev@gmail.com>
2016-02-06 16:37:12 +0200

    Add day controller and view

---
+++ app/controllers/day_controller.rb
@@ -0,0 +1,20 @@
+class DayController < ApplicationController
+  get '/:date?' do
+    @date_string = params[:date] || Date.today.to_s
+    date = Date.parse(@date_string)
+    @lists = ListRepository.find_by_date(date)
+
+    slim :'day/day'
+  end
+
+  post '/:date/list' do
+    ListRepository.create_empty(params[:title], params[:date])
+  end
+
+  private
+
+  def get_date(params)
+    date_string = params[:date] || Date.today.to_s
+    Date.parse(date_string)
+  end
+end
