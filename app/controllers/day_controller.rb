2391eb5f71f9f2492c55aa5debc00f9789a2204f
Dimitar Uzunov <dimitar.uzunov.dev@gmail.com>
2016-02-07 20:12:58 +0200

    Refactor routes by adding after filter

--- app/controllers/day_controller.rb
+++ app/controllers/day_controller.rb
@@ -2,6 +2,8 @@ class DayController < ApplicationController
   get '/:date?', authentication: true do |date_string|
     @date_string = date_string || Date.today.to_s
     date = Date.parse(@date_string)
+
+    @todo_list = TodoList.find_by_date(date, user_id)
     @lists = List.find_by_date(date, user_id)
 
     slim :'day/index'
@@ -9,25 +11,23 @@ class DayController < ApplicationController
 
   post '/:date/list', authentication: true do |date|
     List.create_empty(params[:title], date, user_id)
-    redirect to "/#{date}"
   end
 
   delete '/:date/list/:id', authentication: true do |date, id|
     List.delete_by_id(id, user_id)
-    redirect to "/#{date}"
   end
 
   post '/:date/list/:id', authentication: true do |date, id|
     list = List.find_by_id(id, user_id)
     list.add(params[:text]) if list
-
-    redirect to "/#{date}"
   end
 
   delete '/:date/list/:id/:item_id', authentication: true do |date, list_id, item_id|
     list = List.find_by_id(list_id, user_id)
     list.remove(item_id) if list
+  end
 
+  after '/:date/*' do |date, *|
     redirect to "/#{date}"
   end
 end
