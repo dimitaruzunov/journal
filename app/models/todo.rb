f1f5bd29b9356ca6fc09430aaf564e1f22f83e74
Dimitar Uzunov <dimitar.uzunov.dev@gmail.com>
2016-02-10 17:31:54 +0200

    Add methods for completing and updating a todo

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -2,6 +2,15 @@ class Todo < Item
   include Mongoid::Document
 
   field :done, type: Boolean
-  field :date, type: DateTime
+  field :time, type: Time
   embedded_in :todo_list
+
+  def toggle_done
+    update_attribute(:done, !done)
+  end
+
+  def update(text, hour, min)
+    updated_time = Time.new(time.year, time.month, time.day, hour, min)
+    update_attributes(text: text, time: updated_time)
+  end
 end
